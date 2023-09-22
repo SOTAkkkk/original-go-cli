dc009676cc5006dc944c36a553ce8d114a105d93
SOTAkkkk <>
2023-09-22 10:49:47 +0900

    add cli

--- main.go
+++ main.go
@@ -3,35 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
-	"github.com/fatih/color"
+	"github.com/spf13/cobra"
 )
 
+var rootCmd = &cobra.Command{
+	Use: "hello-cli",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello, World")
+	},
+}
+
 func main() {
-	// カレントディレクトリをデフォルトとして設定し、
-	// コマンドライン引数でディレクトリが指定されている場合はそのディレクトリを使用する
-	dir := "."
-	if len(os.Args) > 1 {
-		dir = os.Args[1]
-	}
-
-	// 指定されたディレクトリ内のファイル・ディレクトリ一覧を取得する
-	files, err := os.ReadDir(dir)
-	// エラーハンドリング: ファイル一覧を取得できなかった場合、エラーメッセージを表示して終了する
+	err := rootCmd.Execute()
 	if err != nil {
-		fmt.Println("Error:", err)
 		os.Exit(1)
-	}
-
-	// 取得したファイル一覧をループで処理する
-	for _, file := range files {
-		fileName := file.Name()
-		// ファイル名の末尾が ".txt" である場合は赤文字で表示、そうでない場合は通常の色で表示
-		if strings.HasSuffix(fileName, ".txt") {
-			color.Red(fileName)
-		} else {
-			fmt.Println(fileName)
-		}
 	}
 }
